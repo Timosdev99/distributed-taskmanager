9856c90159fcdb2982bad685c15f1667e94c58c0
timosdev99 <timothyisah4@gmail.com>
2025-02-04 15:11:15 +0100

    added RBAC to getall users

--- src/ROUTES/userroute.ts
+++ src/ROUTES/userroute.ts
@@ -1,10 +1,11 @@
 import { signUp, login, allUser,  getUser } from "../controllers/usercontroller";
 import { Router } from "express";
 import { authToken } from "../middleware/auth";
+import { ManagerandAdmin } from "../middleware/rbac";
 const router = Router()
 
 router.post("/SignUp", signUp)
 router.post("/login", login)
 router.get("/getuser/:id", authToken, getUser)
-router.get("/alluser", authToken, allUser)
+router.get("/alluser", authToken, ManagerandAdmin, allUser)
 export default router
